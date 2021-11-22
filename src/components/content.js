e41c70f9a09b2def318e6557474c032f228c9663
Vinícius Sizílio <vinicius.sizilio@gmail.com>
2021-11-22 11:33:08 -0300

    novo layout

--- src/components/content.js
+++ src/components/content.js
@@ -1,65 +1,83 @@
 import Styles from '../../styles/scss/content.module.scss'
 import Image from 'next/image'
-import Link from 'next/link'
 
 export default function Content() {
     const url = process.env.NODE_ENV == 'production' ? 'https://correioscep.vercel.app/api/cep/' : 'http://localhost:3000/api/cep/'
 
     return (
-        <div className="container mt-4 mt-md-5 mb-4 mb-md-5 p-3 p-md-5 d-flex justify-content-center">
+        <div className="container mt-4 mt-md-5 mb-4 mb-md-5 p-3 p-md-5">
             <div className="row">
-                <div className="col-12">
-                    <div className={Styles.card + " p-5"}>
-                        <div className="d-flex flex-column justify-content-center align-items-center">
-                            <h1 className={Styles.name + " text-uppercase"}>Correios API - CEP</h1>
-                            <h2 className={Styles.idd}>Busca de endereço pelo CEP</h2>
-                            <code className={Styles.text + " w-100 my-4"}>
-                                <p>Para usar a API basta acessar url <u>{url}</u> com o CEP no final.</p>
-                                <p className="mb-0">
-                                    Exemplo:
+                <div className="col-12 col-md-6 offset-md-3">
+                    <div className={Styles.card + " shadow"}>
+                        <div className="pt-5 px-3 px-md-5 text-center">
+                            <Image
+                                src="/correios.png"
+                                alt="Correios"
+                                width={241}
+                                height={50}
+                                className={Styles.image}
+                            />
+                            <h1 className={Styles.name + " mt-4 text-uppercase"}>Correios API - CEP</h1>
+                            <h2 className={Styles.idd + " mb-0"}>Busca de endereço pelo CEP</h2>
+                            <code className={Styles.text + " d-block w-100 my-4"}>
+                                <p>API gratuita e de alto desempenho para consultar <b>Códigos de Endereçamento Postal (CEP)</b> do Brasil.</p>
+                                <p>O endereço é buscado em três banco de dados (webservices) diferentes, o primeiro que retornar é exibido na requisição.</p>
+                                <p>Para receber dados da API, um CEP no formato de 8 dígitos deve ser fornecido.</p>
+                                <p>
+                                    Exemplo: 
                                     <b>
-                                        <a href={url + '39403400'} target="_blank" rel="noreferrer">
-                                            {url + '39403400'}
+                                        <a href={url + '39403000'} target="_blank" rel="noreferrer">
+                                            {url + '39403000'}
                                         </a>
                                     </b>.
                                 </p>
+                                <p className="mb-0">O retorno será em JSON, algo assim:</p>
+                                <p className="mb-0">
+                                    <Image
+                                        src="/return.png"
+                                        alt="Retorno API Correios CEP"
+                                        width={389}
+                                        height={158}
+                                        className={Styles.image}
+                                    />
+                                </p>
                             </code>
-                            <div className="row w-100">
-                                <div className="col p-0 d-flex justify-content-center">
-                                    <div className={Styles.label + " d-inline-flex align-items-center justify-content-center"}>
-                                        <span>Feito com</span>
-                                        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-                                            <Image
-                                                src="/nextjs.png"
-                                                alt="NextJS"
-                                                width={100}
-                                                height={40}
-                                                className={Styles.image}
-                                            />
-                                        </a>
-                                    </div>
+                        </div>
+                        <div className="d-flex flex-column flex-md-row w-100">
+                            <div className="col-12 col-md-4 p-0 d-flex justify-content-center">
+                                <div className={Styles.label + " d-inline-flex align-items-center justify-content-center"}>
+                                    <span>Feito com</span>
+                                    <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+                                        <Image
+                                            src="/nextjs.png"
+                                            alt="NextJS"
+                                            width={67}
+                                            height={40}
+                                            className={Styles.image}
+                                        />
+                                    </a>
                                 </div>
-                                <div className="col p-0 d-flex justify-content-center">
-                                    <div className={Styles.label + " d-inline-flex align-items-center justify-content-center"}>
-                                        <span>Hospedado em</span>
-                                        <a href="https://vercel.com/" target="_blank" rel="noreferrer">
-                                            <Image
-                                                src="/vercel.png"
-                                                alt="Vercel"
-                                                width={100}
-                                                height={40}
-                                                className={Styles.image}
-                                            />
-                                        </a>
-                                    </div>
+                            </div>
+                            <div className="col-12 col-md-4 p-0 d-flex justify-content-center">
+                                <div className={Styles.label + " d-inline-flex align-items-center justify-content-center"}>
+                                    <span>Hospedado em</span>
+                                    <a href="https://vercel.com/" target="_blank" rel="noreferrer">
+                                        <Image
+                                            src="/vercel.png"
+                                            alt="Vercel"
+                                            width={110}
+                                            height={40}
+                                            className={Styles.image}
+                                        />
+                                    </a>
                                 </div>
-                                <div className="col p-0 d-flex justify-content-center">
-                                    <div className={Styles.label + " d-inline-flex align-items-center justify-content-center"}>
-                                        <span>Criado por</span>
-                                        <a href="https://sizilio.vercel.app" target="_blank" rel="noreferrer">
-                                            Vinícius Sizílio
-                                        </a>
-                                    </div>
+                            </div>
+                            <div className="col-12 col-md-4 p-0 d-flex justify-content-center">
+                                <div className={Styles.label + " d-inline-flex align-items-center justify-content-center"}>
+                                    <span>Criado por</span>
+                                    <a href="https://sizilio.vercel.app" target="_blank" rel="noreferrer">
+                                        Vinícius Sizílio
+                                    </a>
                                 </div>
                             </div>
                         </div>
